5353ede48bfe12b9aa88be758915d42a5a6a89ac
David Aguiar <siempredelao@gmail.com>
2020-05-26 20:37:32 +0200

    Formato de fecha legible para el Calendar frame

--- input_calendar.py
+++ input_calendar.py
@@ -1,3 +1,5 @@
+import locale
+
 from tkcalendar import DateEntry
 from tkinter import *
 import datetime
@@ -28,8 +30,7 @@ class Calendar(Frame):
                                        ipadx=CELL_MARGIN,
                                        ipady=CELL_MARGIN,
                                        sticky=W + E + N + S)
-        # ¿Cómo mierda hago para que la fecha sea´"día/mes/año" y no como la tienen los ingleses?
-        self.calendar_entry = DateEntry(self,
+        self.calendar_date_entry = DateEntry(self,
                                         width=5,
                                         justify="center",
                                         background='darkblue',
@@ -40,14 +41,14 @@ class Calendar(Frame):
                                         year=datetime.now().year,
                                         locale="es_ES",
                                         date_pattern="dd/mm/yyyy")
-        self.calendar_entry.grid(row=1,
+        self.calendar_date_entry.grid(row=1,
                                  column=0,
                                  padx=(0, CELL_PADDING),
                                  pady=CELL_PADDING,
                                  ipadx=CELL_MARGIN,
                                  ipady=CELL_MARGIN,
                                  sticky=W + E + N + S)
-        self.calendar_entry.bind("<<DateEntrySelected>>", self.__print_name_day)
+        self.calendar_date_entry.bind("<<DateEntrySelected>>", self.__print_name_day)
 
         self.calendar_title_label = Label(self,
                                           text="Día",
@@ -76,9 +77,9 @@ class Calendar(Frame):
     def __print_name_day(self, event):
         w = event.widget
         date = w.get_date()
-        date_with_format = date.strftime("%d/%m/%Y")
+        locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
+        date_with_format = date.strftime("%A, %-d de %B de %Y")
         self.day_number.set(date_with_format)
 
     def return_date(self):
-
-        return str(self.day_number.get())
+        return str(self.calendar_date_entry.get_date())
